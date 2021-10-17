6690461ca58e49b994f77f6221074ce4f40e9aa1
MassterKek <trevorwardkun@gmail.com>
2021-10-17 13:45:13 -0400

    Switched over from Pug to Handlebars
    Switched HTML templating from Pug to Handlebars.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const handlebars = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +12,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+  layoutsDir: path.join(__dirname, '/views/layouts'),
+  defaultLayout: "index",
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,19 +28,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  const message = err.message;
+  const error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {message, error});
 });
 
 module.exports = app;
