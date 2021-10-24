9f0e5f25d14de0cc6a1270fad3717413dd4c786c
Nikita Voronin <voro0612@gmail.com>
2021-10-23 20:10:23 -0400

    Revert "events test added"

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const handlebars = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var eventsRouter = require('./routes/events');
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/events', eventsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- utils/consts.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    EVENT_API_KEY: '556f3765b8e52bb40cb155cdfadbe199776da4675c6145bf875354defa83303c',
-    EVENT_SEARCH_BASE_URL: 'https://serpapi.com/search.json',
-    EVENT_SEARCH_ENGINE: 'google_events',
-}
