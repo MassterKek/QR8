a40bf9e3b598fe4d0779a8a6c121a7574c254649
MassterKek <trevorwardkun@gmail.com>
2021-10-25 02:26:52 -0400

    Began Tooling Front-End for style.css
    Began setting up partials to work with the provided style sheets, but still need to figure out if this is the right way to be doing this.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("index route");
-  res.render('main', { title: 'Express'});
+  res.render('main', { title: 'QR8 - Home'});
 });
 
 module.exports = router;
