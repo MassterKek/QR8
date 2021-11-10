2dfee7400785148e77575e15ac503e09651adaec
nvoronin1337 <voro0612@gmail.com>
2021-11-10 10:50:08 -0500

    begin connecting ui with backend

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
+const axios = require('axios');
 var router = express.Router();
-
 
 const event = {
   title: "Event 1",
@@ -9,6 +9,7 @@ const event = {
   covid: "Yes",
   weather: "Cloudy"
 };
+
 const events = [
   event,
   event,
@@ -41,9 +42,15 @@ const events = [
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("index route");
-  res.render('pages/home', { title: 'QR8 - Home' , items:events});
+router.get('/', async function(req, res, next) {
+  // probably need to call this when submit button is clicked
+  axios.post('http://localhost:5000/scraper/v1/event/fetch_one', { q: 'nature', loc: 'boston, ma' })
+    .then((result) => {
+      // now result.data is an array of our events 
+      res.render('pages/home', { title: 'QR8 - Home' , items:events});
+    }).catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
